4c6c764683db8fce5ac7884206455ef96aea1677
Neerav Panchal <neeravpanchal26@gmail.com>
2024-02-11 00:23:50 +0000

    Create a Post request that takes one argument of ToDoItem and creates a new ToDoItem.

--- TodoList_Api/Controllers/TodoListController.cs
+++ TodoList_Api/Controllers/TodoListController.cs
@@ -41,5 +41,14 @@ namespace TodoListAPI.Controllers
 
             return todoItem;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
     }
 }
