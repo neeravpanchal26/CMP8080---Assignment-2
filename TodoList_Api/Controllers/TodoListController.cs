1d1e2af44e8fa16097b2fe804983c3c04925a46f
Neerav Panchal <neeravpanchal26@gmail.com>
2024-02-11 00:20:54 +0000

    Create a Get request that takes no arguments and returns all ToDoItems that have no CompletedDate set.

---
+++ TodoList_Api/Controllers/TodoListController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoListAPI.Data;
+using TodoListAPI.Models;
+
+namespace TodoListAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoItemsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            var todoItems = await _context.TodoItems
+                .Where(t => t.CompletedDate == null)
+                .ToListAsync();
+
+            return todoItems;
+        }
+    }
+}
